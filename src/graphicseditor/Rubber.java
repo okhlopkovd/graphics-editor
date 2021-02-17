d871cc5652fd4f21ae136acd6826276452a3a83d
bv <avghmn@gmail.com>
2021-02-17 10:03:12 +0300

    Implement the rubber tool

---
+++ src/graphicseditor/Rubber.java
@@ -0,0 +1,23 @@
+package graphicseditor;
+
+import graphicseditor.Tool;
+
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.BasicStroke;
+
+public class Rubber implements Tool{
+    private int size;
+    private BasicStroke rubberStroke;
+
+    public Rubber(int size) {
+        this.size = size;
+        rubberStroke = new BasicStroke(size);
+    }
+
+    public void paint(Graphics2D g, int oldX, int oldY, int curX, int curY) {
+        g.setStroke(rubberStroke);
+        g.setColor(new Color(255, 255, 255));
+        g.drawLine(oldX, oldY, curX, curY);
+    }
+}

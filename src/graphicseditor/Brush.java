79930ff307c7c4e3ff3bda060740937fb96306c3
bv <avghmn@gmail.com>
2021-02-17 11:25:19 +0300

    fix: Pen and brush use after rubber
    After rubber use the pen and the brush colors were set to white
    
    Now brush and pen paint() methods set a color to black

--- src/graphicseditor/Brush.java
+++ src/graphicseditor/Brush.java
@@ -4,6 +4,7 @@ import graphicseditor.Tool;
 
 import java.awt.Graphics2D;
 import java.awt.BasicStroke;
+import java.awt.Color;
 
 public class Brush implements Tool{
     private int size;
@@ -15,6 +16,7 @@ public class Brush implements Tool{
     }
 
     public void paint(Graphics2D g, int oldX, int oldY, int curX, int curY) {
+        g.setColor(Color.black);
         g.setStroke(brushStroke);
         g.drawLine(oldX, oldY, curX, curY);
     }
--- src/graphicseditor/Pencil.java
+++ src/graphicseditor/Pencil.java
@@ -2,10 +2,17 @@ package graphicseditor;
 
 import graphicseditor.Tool;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.BasicStroke;
+import java.awt.Graphics2D;
 
 public class Pencil implements Tool {
+    private int size=1;
+    private BasicStroke penStroke = new BasicStroke(1);
+
     public void paint(Graphics2D g, int oldX, int oldY, int curX, int curY) {
+        g.setColor(Color.black);
+        g.setStroke(penStroke);
         g.drawLine(oldX, oldY, curX, curY);
     }
 }
